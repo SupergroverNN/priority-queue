834f8e756ad1cfb73676398531ba10ea0d643afa
Igor <booger1@yandex.ru>
2019-10-16 20:58:16 +0300

    feat: add some Node functionality

--- src/node.js
+++ src/node.js
@@ -1,10 +1,11 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data = data;
+		this.priority = priority;
 	}
 
 	appendChild(node) {
-
+		
 	}
 
 	removeChild(node) {
--- src/queue.js
+++ src/queue.js
@@ -2,7 +2,8 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+		this.data = [];
+		this.maxSize = maxSize;
 	}
 
 	push(data, priority) {
