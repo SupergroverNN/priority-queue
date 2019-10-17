f53ee67c88cef995a74678e20bcbd9941089b1b3
Igor <booger1@yandex.ru>
2019-10-17 22:17:12 +0300

    feat: add Node append, removeChild, remove

--- src/node.js
+++ src/node.js
@@ -2,22 +2,42 @@ class Node {
 	constructor(data, priority) {
 		this.data = data;
 		this.priority = priority;
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
-		
+		if(this.left === null) {
+			node.parent = this;
+			this.left = node
+		} else if (this.right === null) {
+			node.parent = this;
+			this.right = node;
+		} 
 	}
 
 	removeChild(node) {
-
+		if(this.left.data === node.data && this.left.priority === node.priority){
+			node.parent = null;
+			this.left = null
+		} else 	if(this.right.data === node.data && this.right.priority === node.priority){
+			node.parent = null;
+			this.right = null
+		} else {
+			throw Error()
+		}
 	}
 
 	remove() {
-
+		if(this.parent !== null) {
+			this.parent.removeChild(this)
+		}
 	}
 
 	swapWithParent() {
-		
+		let curParent = this.parent;
+		let curChild = this;
 	}
 }
 
