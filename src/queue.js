7661614b0f15eab3407572b274dbe9914b38cc36
Igor <booger1@yandex.ru>
2019-10-20 00:00:12 +0300

    feat: add some heap and queue functionality

--- src/queue.js
+++ src/queue.js
@@ -1,26 +1,31 @@
-const MaxHeap = require('./max-heap.js');
+const MaxHeap = require("./max-heap.js");
 
 class PriorityQueue {
-	constructor(maxSize) {
-		this.data = [];
-		this.maxSize = maxSize;
+  constructor(maxSize) {
+    this.heap = new MaxHeap();
+    this.maxSize = maxSize;
+  }
+
+  push(data, priority) {
+    if (this.heap.length === this.maxSize) throw new Error();
+    this.heap.push(data, priority);
+  }
+
+  shift() {
+		if (this.heap.length === 0) {
+			throw new Error()
+		} else {
+			return this.heap.pop();
+		}
 	}
 
-	push(data, priority) {
+  size() {
+    return this.heap.length;
+  }
 
-	}
-
-	shift() {
-
-	}
-
-	size() {
-
-	}
-
-	isEmpty() {
-		
-	}
+  isEmpty() {
+    return this.heap.length === 0;
+  }
 }
 
 module.exports = PriorityQueue;
