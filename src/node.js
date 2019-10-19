34be11bf931dc6e748dbab52617cacbb79017daa
Igor <booger1@yandex.ru>
2019-10-19 23:39:09 +0300

    feat: add node functionality

--- src/node.js
+++ src/node.js
@@ -1,44 +1,89 @@
 class Node {
-	constructor(data, priority) {
-		this.data = data;
-		this.priority = priority;
-		this.parent = null;
-		this.left = null;
-		this.right = null;
-	}
+  constructor(data, priority) {
+    this.data = data;
+    this.priority = priority;
+    this.parent = null;
+    this.left = null;
+    this.right = null;
+  }
 
-	appendChild(node) {
-		if(this.left === null) {
-			node.parent = this;
-			this.left = node
-		} else if (this.right === null) {
-			node.parent = this;
-			this.right = node;
-		} 
-	}
+  appendChild(node) {
+    if (this.left === null) {
+      node.parent = this;
+      this.left = node;
+    } else if (this.right === null) {
+      node.parent = this;
+      this.right = node;
+    }
+  }
 
-	removeChild(node) {
-		if(this.left.data === node.data && this.left.priority === node.priority){
-			node.parent = null;
-			this.left = null
-		} else 	if(this.right.data === node.data && this.right.priority === node.priority){
-			node.parent = null;
-			this.right = null
-		} else {
-			throw Error()
-		}
-	}
+  removeChild(node) {
+    if (this.left === node) {
+      this.left = null;
+      node.parent = null;
+    } else if (this.right === node) {
+      this.right = null;
+      node.parent = null;
+    } else {
+      throw Error();
+    }
+  }
 
-	remove() {
-		if(this.parent !== null) {
-			this.parent.removeChild(this)
-		}
-	}
+  remove() {
+    if (this.parent !== null) {
+      this.parent.removeChild(this);
+    }
+  }
 
-	swapWithParent() {
-		let curParent = this.parent;
-		let curChild = this;
-	}
+  swapWithParent() {
+    if (this.parent) {
+      // если родитель есть
+      let curParent = this.parent; // родитель
+      let curParentOfParent = curParent.parent; // дед
+      curParent.parent = this; // меняем деда на текущую ноду
+      let curParentRight = curParent.right;
+      let curParentLeft = curParent.left;
+      if (curParent.right === this) {
+        curParent.right = this.right;
+        curParent.left = this.left;
+        this.right = curParent;
+        this.left = curParentLeft;
+        if (curParent.left) {
+          curParent.left.parent = curParent;
+        }
+        if (curParent.right) {
+          curParent.right.parent = curParent;
+        }
+        if (this.left) {
+          this.left.parent = this;
+        }
+      } else if (curParent.left === this) {
+        curParent.right = this.right;
+        curParent.left = this.left;
+        this.right = curParentRight;
+        this.left = curParent;
+        if (curParent.left) {
+          curParent.left.parent = curParent;
+        }
+        if (curParent.right) {
+          curParent.right.parent = curParent;
+        }
+        curParent.parent = this;
+        if (this.right) {
+          this.right.parent = this;
+        }
+      }
+      this.parent = curParentOfParent;
+      if (curParentOfParent && curParent === curParentOfParent.left) {
+        curParentOfParent.left = this;
+      } else if (curParentOfParent && curParent === curParentOfParent.right) {
+        curParentOfParent.right = this;
+      }
+      this.parent = curParentOfParent;
+    } else {
+      return;
+    }
+  }
 }
 
 module.exports = Node;
